ceec905b017cda4ed202e1d321f02c9be481ecd3
David Castellanos <davcastellanoslarios@gmail.com>
2023-10-12 01:05:49 -0600

    create working

---
+++ src/components/Form.jsx
@@ -0,0 +1,70 @@
+import { getTypeApi } from "../utils/data";
+import { useEffect, useState, useRef } from "react";
+import {
+  FormControl,
+  FormLabel,
+  Select,
+  Input,
+  Stack,
+  Button,
+} from "@chakra-ui/react";
+
+export const Form = ({ onClose }) => {
+  const [form, setForm] = useState({
+    name: "",
+    typeId: "",
+  });
+  const [type, setfirst] = useState([]);
+  const formRef = useRef();
+
+  useEffect(() => {
+    getTypeApi().then((types) => setfirst(types));
+  }, []);
+
+  const onChange = (e) => {
+    e.preventDefault();
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await fetch("http://localhost:8080/pokeapi/pokemon", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    })
+  };
+
+  return (
+    <form ref={formRef} onSubmit={handleSubmit}>
+      <FormControl>
+        <FormLabel>Name: </FormLabel>
+        <Input
+          type="text"
+          name="name"
+          value={form.name}
+          onChange={onChange}
+          placeholder="Nombre"
+        />
+        <FormLabel>Type: </FormLabel>
+        <Select onChange={onChange} value={form.typeId} name="typeId">
+          {type.map((e) => (
+            <option key={e.idTypes} value={e.idTypes}>
+              {e.typeName}
+            </option>
+          ))}
+        </Select>
+        <Stack className="mt-4">
+          <Button colorScheme="blue" type="submit" onClick={onClose}>
+            Create
+          </Button>
+          <Button variant="ghost" mr={3} onClick={onClose}>
+            Cancel
+          </Button>
+        </Stack>
+      </FormControl>
+    </form>
+  );
+};
--- src/components/ModalPokemon.jsx
+++ src/components/ModalPokemon.jsx
@@ -1,144 +1,28 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useRef, useEffect } from "react";
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Select,
-    Box,
-    Button,
-    useDisclosure,
-    VStack, 
-    ButtonGroup,
-    Heading,   
-    Text
-  } from '@chakra-ui/react'
-
-import { AddIcon } from '@chakra-ui/icons'
-// import { Formik, Field, Form } from 'formik';
-import { Formik, Form } from 'formik'
-import { Textfield } from './ui/TextField'
-import { getTypeApi } from '../utils/data'
-
-export const ModalPokemon = ({finalRef, isOpen, onClose}) => {
-
-        // console.log(getTypeApi())
-
-        const [type, setfirst] = useState([])
-        const [seletedValue, setSelectedValue] = useState('')
-
-        const handleChange = (event ) => {
-          setSelectedValue(event.target.value)
-          // console.log(event.target.value)
-        }
-
-        // console.log(handleChange)
-
-        useEffect(() => {
-          getTypeApi().then(types => setfirst(types))
-        }, [])
-        
-
-        const submitt = (values, actions) => {
-
-          const vals = {...values}
-              actions.resetForm()
-              fetch("http://localhost:8080/pokeapi/pokemon", {
-                  method: "POST",
-                  // credentials:"include",
-                  headers:{
-                      "Content-Type": "application/json"
-                  },
-                  body: JSON.stringify(vals)
-              }).catch( err => {
-                  console.log(err);
-                  return
-              }).then(res => {
-                  if(!res || !res.ok || res.status >= 400){
-                      return
-                  }
-                  return res.json()
-              })
-              .then(data => { 
-                  if(!data) return;
-  
-                  setUser({...data})
-  
-                  if(data.status){
-                      setErrorSigUp(data.status)
-                  }else if( data.loggedIn){
-                      // console.log(data);
-                      navigate('/home')
-                  }
-              })
-      }
-      
-        return (
-          <>
-            
-            {/* <BtnCreate mt={4} onClick={onOpen}/> */}
-            <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>Pokemon Facture</ModalHeader>
-                <ModalCloseButton />
-                
-                <Formik 
-                  initialValues={{ name:"", typeId:""}}
-                  onSubmit={submitt}
-                >
-                  
-                  <VStack 
-                    as={Form} 
-                    w={{base: "90%", md:"350px"}} 
-                    // w={}
-                    // w={{base: "90%", md: "500px"}} 
-                    m="auto" 
-                    justify="center" 
-                    h="50vh"
-                    spacing="1rem"
-                  >
-                      {/* <Heading></Heading> */}
-                      {/* <Text as="p" color="red.500"> { errorSigUp }</Text> */}
-                      {/* <ModalBody> */}
-                      <Textfield label="Pokemon Name" name="name" placeholder="Enter a Pokemon name" autoComplete="off" type="text" />
-                      {/* <Textfield label="Pokemon Type" name="typeId" placeholder="Enter a type for pokemon" autoComplete="off" type="text" /> */}
-                      <Select placeholder='Select option' onChange={handleChange}>
-                      {
-                          type.map( t => {
-                            
-                            return <option name={t.idTypes} value={t.idTypes} key={t.idTypes}>
-                              {
-                                t.typeName
-                              }
-                            </option>
-                          })
-                        }
-                      </Select>
-                      {/* </ModalBody>   */}
-                      {/* <ButtonGroup className='mb-8'> */}
-                      <ButtonGroup className='mt-10' >
-                        {/* <Button colorScheme='blue' mr={3} onClick={onClose}> */}
-                        <Button variant='ghost' mr={3} onClick={onClose}>
-                          Cancel
-                        </Button>
-                        {/* <Button onClick={() => navigate("/register")} >Create Account</Button> */}
-                        <Button  colorScheme='blue' type="submit">Create</Button>
-                      </ButtonGroup>
-                  </VStack>
-                    {/* ------------------------------------------*/}
-                </Formik>
-                
-                {/* <ModalFooter>
-                  
-                  
-                </ModalFooter> */}
-                
-              </ModalContent>
-            </Modal>
-          </>
-        )
-      }
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { Form } from "./Form";
+export const ModalPokemon = ({ finalRef, isOpen, onClose }) => {
+  return (
+    <>
+      <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Pokemon Facture</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Form onClose={onClose}/>
+          </ModalBody>
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
